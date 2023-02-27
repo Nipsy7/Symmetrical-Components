364927658de85cc51ffdc95fb4018f9958be137c
Exoreath <harrisontim2@gmail.com>
2023-02-27 14:11:18 +1100

    independantly moving series; began work on optimising code

--- SymmetricalComponents/app/src/main/java/com/example/symmetricalcomponents/MainActivity.kt
+++ SymmetricalComponents/app/src/main/java/com/example/symmetricalcomponents/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.MotionEvent
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.Viewport
@@ -16,17 +17,36 @@ import com.jjoe64.graphview.series.LineGraphSeries as LineGraphSeries1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var series1: LineGraphSeries1<DataPoint>
-    private lateinit var series2: LineGraphSeries1<DataPoint>
+    private lateinit var phaseOneSeries: LineGraphSeries1<DataPoint>
+    private lateinit var phaseTwoSeries: LineGraphSeries1<DataPoint>
+    private lateinit var phaseThreeSeries: LineGraphSeries1<DataPoint>
     private lateinit var graph: GraphView
     private lateinit var viewport: Viewport
     private lateinit var button: Button
     private val x = mutableListOf(-100.0, -90.0, -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0, 0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0)
     private val y = mutableListOf(-100.0, -90.0, -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0, 0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0)
 
-    private lateinit var previousGraphPoint: DataPoint
+    private lateinit var previousGraphPointPhaseOne: DataPoint
+    private lateinit var previousGraphPointPhaseTwo: DataPoint
+    private lateinit var previousGraphPointPhaseThree: DataPoint
 
     private var previousX: Double = 0.0
     private var previousY: Double = 0.0
+
+    fun setupSeries(series: LineGraphSeries1<DataPoint>, drawDataPoints: Boolean, colour: Color, dataPoint1: DataPoint, dataPoint2: DataPoint) {
+        series.isDrawDataPoints = drawDataPoints
+        series.color = Color.RED
+        series.appendData(
+            dataPoint1,
+            false,
+            100
+        )
+        phaseOneSeries.appendData(
+            dataPoint2,
+            false,
+            100
+        )
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,29 +60,85 @@ class MainActivity : AppCompatActivity() {
         series1.isDrawDataPoints = true
         series1.color = Color.RED
 
-        series2 = LineGraphSeries1()
-        series2.isDrawDataPoints = false
-        series2.color = Color.GREEN
-        series2.appendData(
+        phaseOneSeries = LineGraphSeries1()
+        phaseOneSeries.isDrawDataPoints = true
+        phaseOneSeries.color = Color.GREEN
+        phaseOneSeries.appendData(
             DataPoint(0.0,0.0),
             false,
             100
         )
-        series2.appendData(
+        phaseOneSeries.appendData(
             DataPoint(50.0,50.0),
             false,
             100
         )
 
-        previousGraphPoint = DataPoint(50.0,50.0)
+        phaseTwoSeries = LineGraphSeries1()
+        phaseTwoSeries.isDrawDataPoints = true
+        phaseTwoSeries.color = Color.BLUE
+        phaseTwoSeries.appendData(
+            DataPoint(-50.0,0.0),
+            false,
+            100
+        )
+        phaseTwoSeries.appendData(
+            DataPoint(0.0,0.0),
+            false,
+            100
+        )
+
+        phaseThreeSeries = LineGraphSeries1()
+        phaseThreeSeries.isDrawDataPoints = true
+        phaseThreeSeries.color = Color.RED
+        phaseThreeSeries.appendData(
+            DataPoint(0.0,0.0),
+            false,
+            100
+        )
+        phaseThreeSeries.appendData(
+            DataPoint(10.0,0.0),
+            false,
+            100
+        )
+
+        previousGraphPointPhaseOne = DataPoint(50.0,50.0)
+        previousGraphPointPhaseTwo = DataPoint(-50.0,0.0)
+        previousGraphPointPhaseThree = DataPoint(10.0,-30.0)
 
         //graph.addSeries(series1)
-        graph.addSeries(series2)
+        graph.addSeries(phaseOneSeries)
+        graph.addSeries(phaseTwoSeries)
+        graph.addSeries(phaseThreeSeries)
 
-        /*var activeSeries: LineGraphSeries1<DataPoint>? = null
-        series2.setOnDataPointTapListener(OnDataPointTapListener { _, _ ->
-            activeSeries = series2
-        })*/
+        val seriesArr = arrayOf(phaseOneSeries, phaseTwoSeries, phaseThreeSeries)
+        val previousCoord = arrayOf(previousGraphPointPhaseOne, previousGraphPointPhaseTwo, previousGraphPointPhaseThree)
+        var seriesArrIndex = 0
+
+        phaseOneSeries.setOnDataPointTapListener(OnDataPointTapListener { _, _ ->
+            seriesArrIndex = 0
+            Toast.makeText(
+                applicationContext,
+                "series: PhaseOneSeries",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+        phaseTwoSeries.setOnDataPointTapListener(OnDataPointTapListener { _, _ ->
+            seriesArrIndex = 1
+            Toast.makeText(
+                applicationContext,
+                "series: PhaseTwoSeries",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+        phaseThreeSeries.setOnDataPointTapListener(OnDataPointTapListener { _, _ ->
+            seriesArrIndex = 2
+            Toast.makeText(
+                applicationContext,
+                "series: PhaseThreeSeries",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
 
         graph.setOnTouchListener { v, event ->
 
@@ -77,14 +153,13 @@ class MainActivity : AppCompatActivity() {
                     dx*=0.2
                     dy*=0.2
 
-                    val values = arrayOf(DataPoint(0.0,0.0), DataPoint(previousGraphPoint.x + dx, previousGraphPoint.y - dy))
-                    previousGraphPoint = DataPoint(previousGraphPoint.x + dx, previousGraphPoint.y - dy)
-
+                    val values = arrayOf(DataPoint(0.0, 0.0), DataPoint(previousCoord[seriesArrIndex].x + dx, previousCoord[seriesArrIndex].y - dy))
+                    previousCoord[seriesArrIndex] = DataPoint(previousCoord[seriesArrIndex].x + dx, previousCoord[seriesArrIndex].y - dy)
                     if (values[0].x > values[1].x) {
-                        series2.resetData(arrayOf(values[1], values[0]))
+                        seriesArr[seriesArrIndex].resetData(arrayOf(values[1], values[0]))
                     }
                     else {
-                        series2.resetData(arrayOf(values[0], values[1]))
+                        seriesArr[seriesArrIndex].resetData(arrayOf(values[0], values[1]))
                     }
                 }
             }
@@ -100,36 +175,6 @@ class MainActivity : AppCompatActivity() {
         }
         makeGraph(true)
     }
-
-
-
-    /*override fun onTouchEvent(event: MotionEvent): Boolean {
-        val x: Float = event.x
-        val y: Float = event.y
-
-        when (event.action) {
-            MotionEvent.ACTION_MOVE -> {
-
-                var dx: Double = x - previousX
-                var dy: Double = y - previousY
-
-                val values = arrayOf(DataPoint(0.0,0.0), DataPoint(previousGraphPoint.x + dx, previousGraphPoint.y - dy))
-                previousGraphPoint = DataPoint(previousGraphPoint.x + dx, previousGraphPoint.y - dy)
-
-                if (values[0].x > values[1].x) {
-                    series2.resetData(arrayOf(values[1], values[0]))
-                }
-                else {
-                    series2.resetData(arrayOf(values[0], values[1]))
-                }
-
-            }
-        }
-
-        previousX = x.toDouble()
-        previousY = y.toDouble()
-        return true
-    }*/
 
     private fun makeGraph(appStart: Boolean) {
         // Give x and y axes their range
